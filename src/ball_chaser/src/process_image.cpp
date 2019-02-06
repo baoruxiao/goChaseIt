61bac3228600839376d683667841dd533945d82e
baoruxiao <baoruxiao@hotmail.com>
2019-02-05 22:17:20 -0800

    debug process image

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -29,20 +29,20 @@ void process_image_callback(const sensor_msgs::Image img)
     unsigned int width = img.width;
     int white_pixel = 255;
     std::vector<unsigned> pos_xs = {};
-    for (unsigned i = 0; i < img.height; i++) {
+    for (unsigned i = 0; i < img.height*img.step; i++) {
       for (unsigned j = 0; j < img.width; i++) {
         if (img.data[i*img.step+j] == white_pixel) pos_xs.push_back(j);
       }
     }
-    if (pos_xs.size() == 0) return;
-    unsigned pos_x_mean = std::accumulate(pos_xs.begin(), pos_xs.end(), 0.0)/width;
-    if (pos_x_mean < width / 3) {
-      drive_robot(0.5, 0.0);
-    } else if (pos_x_mean < 2 * width / 3) {
-      drive_robot(0.5, 0.5);
-    } else {
-      drive_robot(0.5, -0.5);
-    }
+    // if (pos_xs.size() == 0) return;
+    // unsigned pos_x_mean = std::accumulate(pos_xs.begin(), pos_xs.end(), 0.0)/width;
+    // if (pos_x_mean < width / 3) {
+    //   drive_robot(0.5, 0.0);
+    // } else if (pos_x_mean < 2 * width / 3) {
+    //   drive_robot(0.5, 0.5);
+    // } else {
+    //   drive_robot(0.5, -0.5);
+    // }
     return;
 }
 
